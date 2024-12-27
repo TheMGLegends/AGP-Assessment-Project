79484c427eb9aacb27879dec4bc527c1839768f5
TheMGLegends <matejovo34@gmail.com>
2024-12-27 17:22:37 +0000

    Added All V/P Shaders
    Still have some redundant VIn members and such

---
+++ Shaders/SkyboxVertexShader.hlsl
@@ -0,0 +1,30 @@
+cbuffer SkyboxBuffer // UnlitBuffer
+{
+	matrix wvp;
+};
+
+struct VIn
+{
+	float4 position : POSITION;
+	float4 colour : COLOR0;
+	float2 uv : TEXCOORD0;
+	float3 normal : NORMAL;
+};
+
+struct VOut
+{
+    float4 position : SV_POSITION;
+    float4 colour : COLOR0;
+    float3 uvw : TEXCOORD0;
+};
+
+VOut main(VIn input)
+{
+    VOut output;
+    
+    output.position = mul(wvp, input.position);
+    output.colour = input.colour;
+    output.uvw = input.position.xyz;
+
+    return output;
+}
